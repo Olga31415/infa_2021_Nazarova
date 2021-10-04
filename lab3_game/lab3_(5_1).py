9fe4841fcfdfd9548c86ac91f282ed17c6c8760a
Nazarova Sofia <nazarova.sf@phystech.edu>
2021-10-04 21:36:41 +0300

    hw

--- lab3_game/lab3_(5_1).py
+++ lab3_game/lab3_(5_1).py
@@ -1,40 +1,76 @@
-import pygame as pg
+import pygame
 
-pg.init()
+pygame.init()
 
 FPS = 30
-screen = pg.display.set_mode((820, 550))
+(length, height) = (1250, 834)
+screen = pygame.display.set_mode((length, height))
 
-sky = (254, 213, 162)
-sun = (252, 238, 33)
-cloud = (254, 213, 196)
-back = (252, 152, 49)
-front = (172, 67, 52)
-sand = (179, 134, 148)
-
-
-
-screen.fill(sky)
-
-back_x=[(0, 280), (10, 240), (180, 130), (210, 140), (220, 150), (320, 235), (360, 230), (400, 240),(440, 200), (480, 220), (510, 210), (600, 130), (630, 160), (660, 170), (710, 180), (750,140), (820, 200)]
-
-pg.draw.rect(screen, cloud, (0, 150, 820, 130)) #облака
-pg.draw.circle(screen, sun, (400, 150), 50) #солнце
-pg.draw.rect(screen, sand, (0, 350 , 820, 200)) #песок
-pg.draw.polygon(screen, back, back_x)
-pg.draw.ellipse(screen, back, [(660, 170), ()] )
+front_color = (180, 135, 149)
+back_color = (254, 214, 149)
+sky_color = (254, 214, 163)
+hight_color = (254, 214, 197)
+back_mountains_color = (252, 153, 45)
+front_mountains_color = (173, 65, 49)
+sun_color = (252, 239, 27)
+bird_color = (64, 27, 3)
+mountain_color = (44, 7, 33)
+width = 10
 
 
+pygame.draw.rect(screen, sky_color, (0, 0 * height, length, 0.25 * height))
 
-pg.display.update()
-clock = pg.time.Clock()
+pygame.draw.rect(screen, hight_color, (0, 0.25 * height, length, 0.26 * height))
+pygame.draw.polygon(screen, back_mountains_color, [(0, 0.5 * height),
+                                       (0, 0.4 * height),
+                                       (0.06 * length, 0.38 * height),
+                                       (0.18 * length, 0.2 * height),
+                                       (0.24 * length, 0.24 * height),
+                                       (0.27 * length, 0.3 * height),
+                                       (0.44 * length, 0.45 * height),
+                                       (0.5 * length, 0.4 * height),
+                                       (0.55 * length, 0.39 * height),
+                                       (0.6 * length, 0.42 * height),
+                                       (0.64 * length, 0.4 * height),
+                                       (0.7 * length, 0.35 * height),
+                                       (0.8 * length, 0.24 * height),
+                                       (0.82 * length, 0.2 * height),
+                                       (0.85 * length, 0.21 * height),
+                                       (0.9 * length, 0.34 * height),
+                                       (0.96 * length, 0.3 * height),
+                                       (length, 0.2 * height),
+                                       (length, 0.4 * height)])
+pygame.draw.circle(screen, sun_color, (int(0.5 * length), int(0.25 * height)), 80)
+
+pygame.draw.rect(screen, back_color, (0, 0.5 * height, length, 0.25 * height))
+pygame.draw.ellipse(screen, front_mountains_color, (0.04 * length, 0.45 * height, 200, 400))
+pygame.draw.ellipse(screen, front_mountains_color, (0.62 * length, 0.55 * height, 140, 380))
+pygame.draw.polygon(screen, front_mountains_color, [(0, 0.75 * height),
+                                        (0, 0.55 * height),
+                                        (0.2 * length, 0.75 * height),
+                                        (0.27 * length, 0.6 * height),
+                                        (0.35 * length, 0.7 * height),
+                                        (0.38 * length, 0.55 * height),
+                                        (0.48 * length, 0.6 * height),
+                                        (0.54 * length, 0.7 * height),
+                                        (0.62 * length, 0.68 * height),
+                                        (0.68 * length, 0.55 * height),
+                                        (0.8 * length, 0.65 * height),
+                                        (0.88 * length, 0.55 * height),
+                                        (0.93 * length, 0.6 * height),
+                                        (length, 0.45 * height),
+                                        (length, 0.75 * height)])
+pygame.draw.rect(screen, front_color, (0, 0.75 * height, length, 0.25 * height))
+
+
+pygame.display.update()
+clock = pygame.time.Clock()
 finished = False
 
 while not finished:
     clock.tick(FPS)
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             finished = True
 
-pg.quit()
-pg.quit()
+pygame.quit()
